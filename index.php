a8f027fee46f87dc496b1a8a99131d440f9bf6ba
Ankitkumar Chheda <ankitkumar.chheda@gamil.com>
2017-08-19 13:57:05 +0530

    Initial Repo (Web Starter Kit)

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container">
+    <h1> This is the boiler template for development</h1>
+    <div class="row">
+      <div class="col-md-12" style="background-color:red;color:white">
+        Change folder in Preprose fr Live Reload
+      </div>
+    </div>
+  </div>
+
+  <script src="js/jquery3.2.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/script.js"></script>
+</body>
+</html>
